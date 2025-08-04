ee90dbdfb166271870876cff2e5a893a5b86e19b
Hussain Arif <hussainarifkl@gmail.com>
2025-08-04 13:27:43 +0200

    Initial commit

---
+++ nomad_plugin_demo/tests/parsers/test_parser.py
@@ -0,0 +1,12 @@
+import logging
+
+from nomad.datamodel import EntryArchive
+
+from nomad_plugin_demo.parsers.parser import NewParser
+
+
+def test_parse_file():
+    parser = NewParser()
+    archive = EntryArchive()
+    parser.parse('tests/data/BZ011_Rohdaten.dat', archive, logging.getLogger())
+    assert "Datum" in (archive.data.quantities)[0]
---
+++ nomad_plugin_demo/tests/schema_packages/test_schema_package.py
@@ -0,0 +1,10 @@
+import os.path
+
+from nomad.client import normalize_all, parse
+
+
+def test_schema_package():
+    test_file = os.path.join('tests', 'data', 'BZ011_Rohdaten.dat')
+    entry_archive = parse(test_file)[0]
+    normalize_all(entry_archive)
+    assert len(entry_archive.data.quantities) > 0
