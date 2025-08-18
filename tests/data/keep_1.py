fdf2ebf1d35db0f3fc6124cda4e7ccd394e201d5
Hussain Arif <hussainarifkl@gmail.com>
2025-08-18 12:02:03 +0200

    Update script

--- tests/data/keep_1.py
+++ tests/data/keep_1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 measurement_data_dataframe = pd.read_csv("./BZ011_Rohdaten.dat", sep="\t", decimal=",")
-print(measurement_data_dataframe.columns)
 measurement_data_dataframe = measurement_data_dataframe[
     measurement_data_dataframe["Set Kommentar"] == "0,60V"
 ]
 
+measurement_data_dataframe = measurement_data_dataframe.head(100)
 measurement_data_dataframe.to_csv("./small_BZ011_Rohdaten.dat", sep="\t", decimal=",")
