65fb17565c0ab363d67be598f928400a871be7b1
Hussain Arif <hussainarifkl@gmail.com>
2025-08-18 09:40:23 +0200

    Add type safety and remove incompatible class method

--- tests/data/keep_1.py
+++ tests/data/keep_1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 measurement_data_dataframe = pd.read_csv(
-    "./BZ011_Rohdaten.dat", nrows=5, sep="\t", decimal=","
+    "./BZ011_Rohdaten.dat", nrows=1, sep="\t", decimal=","
 )
 measurement_data_dataframe.to_csv("./small_BZ011_Rohdaten.dat", sep="\t", decimal=",")
